93ca0465d55f3a1736b924decb0247b840f257e5
leesuin0710 <suin3559@gmail.com>
2025-12-23 17:49:45 +0900

    feat: discovery initial settings

---
+++ discovery/src/test/java/outback/the_dollar/disovery/DisoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package outback.the_dollar.disovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
